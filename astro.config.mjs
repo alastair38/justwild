29ddf7906112a013aae28eaeb04df61ef1ae863c
alastair38 <alastair@alastaircox.com>
2022-09-09 21:53:17 +0100

    Removed netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
-import netlify from "@astrojs/netlify/functions";
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
-  output: "server",
-  adapter: netlify()
-});
+})
