57740600c60ae0273fb3fb7e527519dadbf6b82d
alastair38 <alastair@alastaircox.com>
2022-09-15 19:06:56 +0100

    +css to /src -tailwind / compress from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,9 @@
 import {defineConfig} from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
-
-import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://justwild.netlify.app/',
-  integrations: [
-    tailwind(),
-    image(),
-    sitemap(),
-    compress({
-      css: true,
-      html: false,
-      js: false,
-      img: false,
-      svg: false,
-    }),
-  ],
+  integrations: [image(), sitemap()],
 })
